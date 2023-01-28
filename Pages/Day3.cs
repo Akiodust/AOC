701882ec36c4e1a2ac30c6db20961fd3883ade4e
Akiodust <adamek.adulka.kucera@seznam.cz>
2023-01-28 20:02:44 +0100

    finaly added

---
+++ Pages/Day3.cs
@@ -0,0 +1,53 @@
+﻿namespace AOG_blazer.Pages
+{
+    public partial class Day3
+    {
+        public string Input { get; set; }
+        private string[] InputLines { get; set; }
+        private string letterPriority { get; set; } = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public string Output2 { get; set; }
+        public void Solve()
+        {
+            Output2 = string.Empty;
+            InputLines = Input.Split(Environment.NewLine);
+            int priorityPart1 =0;
+            int priorityPart2 = 0;
+            for (int i = 0; i < InputLines.Length; i++)
+            {
+                string firstHalf = InputLines[i].Remove((InputLines[i].Length/2));
+                string secondHalf = InputLines[i].Remove(0, (InputLines[i].Length / 2));
+                foreach(char ch in firstHalf)
+                {
+                    if (secondHalf.Contains(ch))
+                    {
+                        priorityPart1 += letterPriority.IndexOf(ch);
+                        break;
+                    }
+                }
+                if (i % 3 == 0)
+                {
+                    foreach (char ch in InputLines[i])
+                    {
+                        int groupContain = 0;
+                        if(InputLines[i+1].Contains(ch))
+                        {
+                            groupContain++;
+                        }
+                        if (InputLines[i + 2].Contains(ch))
+                        {
+                            groupContain++;
+                        }
+                        if (groupContain == 2) 
+                        {
+                            priorityPart2 += letterPriority.IndexOf(ch);
+                            break;
+                        }
+                    }
+                }
+                
+            }
+            Output2 += priorityPart1.ToString() + Environment.NewLine;
+            Output2 += priorityPart2.ToString() + Environment.NewLine;
+        }
+    }
+}
---
+++ Pages/Day4.cs
@@ -0,0 +1,61 @@
+﻿namespace AOG_blazer.Pages
+{
+    public partial class Day4
+    {
+        public string Input { get; set; }
+        private string[] InputLines { get; set; }
+        public string Output2 { get; set; }
+        public void Solve()
+        {
+            Output2 = string.Empty;
+            InputLines = Input.Split(Environment.NewLine);
+            int fullyContains = 0;
+            int partialyContains = 0;
+            for (int i = 0; i < InputLines.Length; i++)
+            {
+                string interval1 = InputLines[i].Split(',').First();
+                string interval2 = InputLines[i].Split(',').Last();
+                int firstCompare1 = int.Parse(interval1.Split('-').First());
+                int firstCompare2 = int.Parse(interval2.Split('-').First());
+                int secondCompare1 = int.Parse(interval1.Split('-').Last());
+                int secondCompare2 = int.Parse(interval2.Split('-').Last());
+                fullyContains += Compare(firstCompare1, secondCompare1, firstCompare2, secondCompare2);
+                partialyContains += CompareUltra(firstCompare1, secondCompare1, firstCompare2, secondCompare2);
+            }
+            Output2 += fullyContains.ToString() + Environment.NewLine;
+            Output2 += partialyContains.ToString() + Environment.NewLine;
+        }
+        private int Compare(int first, int second, int third, int forth)
+        {
+            if (first <= third && second >= forth)
+            {
+                return 1;
+            }
+            if (third <= first && forth >= second)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        private int CompareUltra(int first, int second, int third, int forth)
+        {
+            if (first <= third && second >= third)
+            {
+                return 1;
+            }
+            if (first <= forth && second >= forth)
+            {
+                return 1;
+            }
+            if (third <= first && forth >= first)
+            {
+                return 1;
+            }
+            if (third <= second && forth >= second)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
