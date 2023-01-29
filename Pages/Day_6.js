0a0d1a0b7ec10552026920c5e30b9407b298c6f1
Akiodust <adamek.adulka.kucera@seznam.cz>
2023-01-29 20:32:38 +0100

    create Blazor project

--- Pages/Day_6.js
+++
@@ -1,6 +0,0 @@
-﻿$("textarea").each(function () {
-    this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
-}).on("input", function () {
-    this.style.height = 0;
-    this.style.height = (this.scrollHeight) + "px";
-});
